42f99a797a4d9b81e7918425ea4af592d6d6f272
tommyeastman1 <tommyeastman1@gmail.com>
2017-11-06 13:47:29 -0300

    submit post form setup

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react'
 import logo from '../logo.svg'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
 
 class Feed extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { content: '' }
+  }
+
+  onSubmit(event) {
+    //prevent default form behavior
+    // event.preventDefault()
+    // this.props.mutate({
+    //   variables: { content: this.state.content }
+    // })
+    console.log(this.state.content)
+  }
+
   render() {
     return (
       <div className='App'>
@@ -10,9 +26,27 @@ class Feed extends Component {
           <h1 className='App-title'>Feed</h1>
         </header>
         <p className='App-intro'>This is the feed</p>
+        <h3>Create a new post</h3>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <label>Post Content:</label>
+          <input
+            value={this.state.content}
+            onChange={event => this.setState({ content: event.target.value })}
+          />
+        </form>
       </div>
     )
   }
 }
 
+const mutation = gql`
+  mutation AddSong($content: String) {
+    addSong(content: $content) {
+      id
+      content
+    }
+  }
+`
+
+//export default graphql(mutation)(Feed)
 export default Feed
