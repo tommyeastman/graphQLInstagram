a5c381b3f8d9612edcb3b4965ef828042ac08608
tommyeastman1 <tommyeastman1@gmail.com>
2017-11-06 13:28:37 -0300

    pull app.js into index.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,5 @@
-import React, { Component } from 'react'
-import { ApolloProvider } from 'react-apollo'
-import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
 
-const httpLink = new HttpLink({
-  uri: 'https://api.graph.cool/simple/v1/cj9ocji231arr0124471yj13z'
-})
-
-const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
-})
-
-class App extends Component {
-  render() {
-    return (
-      <ApolloProvider client={client}>
-        <div className='App'>
-          <header className='App-header'>
-            <img src={logo} className='App-logo' alt='logo' />
-            <h1 className='App-title'>Welcome to React</h1>
-          </header>
-          <p className='App-intro'>Yoooooo</p>
-        </div>
-      </ApolloProvider>
-    )
-  }
+export default ({ children }) => {
+  return <div className='container'>{children}</div>
 }
-
-export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import logo from './logo.svg'
 import './index.css'
-import App from './App'
+import './App.css'
+
+const httpLink = new HttpLink({
+  uri: 'https://api.graph.cool/simple/v1/cj9ocji231arr0124471yj13z'
+})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to React</h1>
+        </header>
+        <p className='App-intro'>Yoooooo</p>
+      </div>
+    </ApolloProvider>
+  )
+}
 //import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Root />, document.querySelector('#root'))
 //registerServiceWorker();
